b09265570df4afed7c7fc8cdea2e3ca82dbed312
lpdacosta <47661481+lpdacosta@users.noreply.github.com>
2019-02-18 09:20:15 -0300

    Add files via upload

---
+++ exerciciocpf.py
@@ -0,0 +1,19 @@
+cpf=12345678910
+print (cpf)
+print ('O meu cpf é',cpf)
+print (type(cpf))
+
+nome='Luciano'
+tel=99874521
+cpf=123456789
+altura=1.80
+
+print ('meu nome é',nome)
+print ('meu telefone é',tel)
+print ('meu cpf é',cpf)
+print ('minha altura é',altura)
+print (type(nome))
+print (type (tel))
+print (type (cpf))
+print (type(altura))
+
